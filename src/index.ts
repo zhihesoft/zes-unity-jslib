70c275d70a5f60f63819bbecada38f0eba523c8a
stanley <stanley@zhihesoft.com>
2022-09-01 18:03:25 +0800

    update v1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { App } from "./lib/app";
 export { getLogger } from "./lib/logger";
-export { page, PageMetaData } from "./lib/metadata_page";
+export { page, PageMetaData, Transit } from "./lib/metadata_page";
 export { LayerService } from "./lib/services/layer_service";
 export { PageService } from "./lib/services/page_service";
 export { ResourceService } from "./lib/services/resource_service";
@@ -8,7 +8,4 @@ export * from "./lib/util";
 export * from "./lib/view_decorators";
 export * from "./lib/view_interfaces";
 export { ViewRef } from "./lib/view_ref";
-
-
-
 
