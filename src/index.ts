56177f0bd63265790593cedf73488de3ef8ec65f
stanley <stanley@zhihesoft.com>
2022-09-02 15:47:45 +0800

    add export of service

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ export { App } from "./lib/app";
 export { ListViewComponent } from "./lib/list_view_component";
 export { getLogger } from "./lib/logger";
 export { page, PageMetaData, Transit } from "./lib/metadata_page";
+export { FadeService } from "./lib/services/fade_service";
 export { LayerService } from "./lib/services/layer_service";
 export { PageService } from "./lib/services/page_service";
 export { ResourceService } from "./lib/services/resource_service";
+export { SystemInfo } from "./lib/services/systeminfo_service";
 export { EaseType } from "./lib/tween/ease_type";
 export { tween } from "./lib/tween/tween";
 export * from "./lib/util";
 export * from "./lib/view_decorators";
 export * from "./lib/view_interfaces";
 export { ViewRef } from "./lib/view_ref";
-
-
-
 
